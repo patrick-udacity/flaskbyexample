18baf2bda76af2eb3b74bc0b4530ac0e44fd5d01
vagrant <vagrant@vagrant.vm>
2018-01-03 23:18:36 +0000

    Adding Jinja templates.

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,6 +1,6 @@
 import feedparser
-from flask import Flask
-
+from flask import Flask, render_template
+import pdb
 app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -13,15 +13,10 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route("/<publication>")
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0] 
-    return """<html>
-                <body>
-                    <h1> """ + publication.upper() + """ Headlines </h1>
-                        <b>{0}</b> <br/>
-                        <i>{1}</i> <br/>
-                        <p>{2}</p> <br/>
-                </body>
-            </html>""".format(first_article.get("title"), first_article. get("published"), first_article.get("summary"))
+    #pdb.set_trace()
+    return render_template("home.html",
+        articles=feed['entries']
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000, debug=True)
