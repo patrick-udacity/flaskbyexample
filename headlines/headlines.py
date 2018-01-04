e8ba8b1e8beb88b3040268e0fe6ddd6c121fd88f
vagrant <vagrant@vagrant.vm>
2018-01-04 22:33:26 +0000

    Adding temperature listing for F and C.

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,8 +1,11 @@
 import feedparser
 from flask import Flask, render_template
 from flask import request
+import json, urllib2, urllib
 import pdb
+
 app = Flask(__name__)
+
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
     'fox': 'http://feeds.foxnews.com/foxnews/latest',
@@ -10,18 +13,55 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
     'powershell': 'http://community.idera.com/powershell/rss',
     'reuters': 'http://feeds.reuters.com/Reuters/worldNews'
 }
+
+#weather APIID: da5610aabb7d8e2e8ec57ef2a74a263c
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=da5610aabb7d8e2e8ec57ef2a74a263c"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=b23c94daab584f4580e4e2bf75cbcf7e"
+
+DEFAULTS = {'publication': 'bbc',
+            'city': 'London,UK',
+            'currency_from': 'GBP',
+            'currency_to': 'USD'
+            }
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
-    else:
-        publication = query.lower()
+def home():
+
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    #This will provide an additional temperature listing in celcius
+    temperatureCel =  (weather['temperature'] - 32) * 5.0/9.0
+    temperatureCel= str(round(temperatureCel, 2))
+
+    return render_template("home.html", articles=articles,
+        weather=weather,
+        temperatureCel=temperatureCel)
+
+def get_news(publication):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    #pdb.set_trace()
-    return render_template("home.html",
-        articles=feed['entries']
-    )
+    return feed['entries']
+
+
+def get_weather(query):
+    query = urllib.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {'description': parsed['weather'][0]['description'],
+                   'temperature': parsed['main']['temp'],
+                   'city': parsed['name'],
+                   'country': parsed['sys']['country']
+                   }
+    return weather
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000, debug=True)
