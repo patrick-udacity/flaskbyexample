01bcf4bede7c1a3d33ef31dead6e3fe4f9c195a7
vagrant <vagrant@vagrant.vm>
2018-01-06 16:26:45 +0000

    Completed work from book. CSS and Cookies added.

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,6 +1,6 @@
+import datetime
 import feedparser
-from flask import Flask, render_template
-from flask import request
+from flask import Flask,  make_response, render_template, request
 import json, urllib2, urllib
 import pdb
 
@@ -29,36 +29,48 @@ DEFAULTS = {'publication': 'bbc',
 def home():
 
     # get customized headlines, based on user input or default
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback('publication')
     articles = get_news(publication)
 
     # get customized weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback('city')
     weather = get_weather(city)
     #This will provide an additional temperature listing in celcius
     temperatureCel =  (weather['temperature'] - 32) * 5.0/9.0
     temperatureCel= str(round(temperatureCel, 2))
 
     # get customised currency based on user input or default
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback('currency_from')
+    currency_to = get_value_with_fallback('currency_to')
     rate, currencies = get_rate(currency_from, currency_to)
 
-    return render_template("home.html", articles=articles,
-        weather=weather,
-        temperatureCel=temperatureCel,
-        currency_from=currency_from, 
-        currency_to=currency_to, rate=rate,
-        currencies=sorted(currencies)
+    response = make_response(
+        render_template("home.html",
+            publication = publication,
+            articles=articles,
+            weather=weather,
+            temperatureCel=temperatureCel,
+            currency_from=currency_from,
+            currency_to=currency_to,
+            rate=rate,
+            currencies=sorted(currencies)
         )
+    )
+    #pdb.set_trace()
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from",
+    currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
+
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
 
 def get_rate(frm, to):
     all_currency = urllib2.urlopen(CURRENCY_URL).read()
