d18cd2f4fa4980c80dd872ecc0569c58c7200150
Patrick Parkison <patrick.udacity@gmail.com>
2018-01-11 21:14:13 +0000

    All book related additions added. I will add more to enhance learning.

--- dinemap/dbhelper.py
+++ dinemap/dbhelper.py
@@ -7,15 +7,6 @@ class DBHelper:
     def connect(self, database="dinemap"):
         return pymysql.connect(host='localhost',user=dbconfig.db_user,passwd=dbconfig.db_password,db=database)
 
-    def get_all_inputs(self):
-        connection = self.connect()
-        try:
-            query = "SELECT description FROM diningEvents;"
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-            return cursor.fetchall()
-        finally:
-            connection.close()
 
     def get_all_diningEvents(self):
         connection = self.connect()
@@ -52,26 +43,6 @@ class DBHelper:
                 connection.commit()
         except Exception as e:
             print(e)
-        finally:
-            connection.close()
-
-    def add_input(self, data):
-        connection = self.connect()
-        try:
-            query = "INSERT INTO diningEvents (description) VALUES (%s);"
-            with connection.cursor() as cursor:
-                cursor.execute(query, data)
-                connection.commit()
-        finally:
-            connection.close()
-
-    def clear_all(self):
-        connection = self.connect()
-        try:
-            query = "DELETE FROM diningEvents;"
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                connection.commit()
         finally:
             connection.close()
 
--- dinemap/dinemap.py
+++ dinemap/dinemap.py
@@ -2,49 +2,75 @@ from dbhelper import DBHelper
 from flask import Flask
 from flask import render_template
 from flask import request
+import dateparser, datetime
 import json
-
+import string
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = dict([
+    ('Asian','Asian'),
+    ('Breakfast','Breakfast'),
+    ('BBQ','BBQ'),
+    ('Cajun','Cajun'),
+    ('Deli','Deli'),
+    ('Italian','Italian'),
+    ('Mexican','Mexican'),
+    ('PubGrub','PubGrub'),
+    ('Seafood','Seafood'),
+    ('Southern','Southern'),
+    ('Other','Other/Unspecified'),
+    ('Burger','Burger')
+])
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
 @app.route("/")
-def home():
+def home(error_message=None):
     try:
         diningEvents = DB.get_all_diningEvents()
         diningEvents = json.dumps(diningEvents)
     except Exception as e:
         print e
         data = e
-    return render_template("home.html", diningEvents=diningEvents)
+    return render_template("home.html", 
+        diningEvents=diningEvents,
+        categories=categories,
+        error_message=error_message
+    )
 
-
-@app.route("/add", methods=["POST","GET"])
-def add():
-    try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print e
-    return home()
 
 @app.route("/submitDiningEvent", methods=['POST'])
 def submitDiningEvent():
     category = request.form.get("category")
+    if category not in categories:
+        return home()
+    
+    #Retrive and validate the date.
     date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
-    DB.add_diningEvent(category, date, latitude, longitude, description)
-    return home()
+    date = format_date(date)
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
 
-@app.route("/clear")
-def clear():
     try:
-        DB.clear_all()
-    except Exception as e:
-        print e
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+
+    description = sanitize_string(request.form.get("description"))
+    DB.add_diningEvent(category, date, latitude, longitude, description)
     return home()
 
 if __name__ == '__main__':
